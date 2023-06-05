fe2ae5210d26847fefb7fc8624edd1da194ebc6e
ZackyNurahman <121843185+ZackyNurahman@users.noreply.github.com>
2023-06-05 09:46:25 +0700

    Add files via upload

---
+++ src/tekido2.js
@@ -0,0 +1,18 @@
+import Content6 from "./content6";
+import Footer from "./footer";
+import Hero3 from "./hero3";
+import Nav from "./nav";
+
+const Tekido2 = () => {
+    return ( 
+        <>
+        <Nav />
+        <Hero3 />
+        <Content6 />
+        <Footer />
+        </>
+     );
+}
+ 
+export 
+default Tekido2;
