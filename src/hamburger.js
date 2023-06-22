069f898b16602e9c16bf813e31952a26978d48cb
ZackyNurahman <121843185+ZackyNurahman@users.noreply.github.com>
2023-06-22 23:58:45 +0700

    Delete src directory

--- src/hamburger.js
+++
@@ -1,9 +0,0 @@
-const HambugerMenu = ({open}) => {
-    return ( 
-        <div className={open?"flex flex-col w-[40vw] h-[100vh] bg-slate-900 absolute top-[5rem] right-0":"hidden"}>
-            <h1>Test</h1>
-        </div>
-     );
-}
- 
-export default HambugerMenu;
